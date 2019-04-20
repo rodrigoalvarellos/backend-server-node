bbd0be4df2d1bf1e473d6a81d68702025e184b4d
Rodrigo Alvarellos <rodri.alvarellos@gmail.com>
2019-04-20 13:05:58 -0300

    Primer Commit

---
+++ app.js
@@ -0,0 +1,42 @@
+// Requires, librerias a utilizar
+var express = require('express');
+var mongoose = require('mongoose');
+
+
+
+// Inicializar variables
+var app = express();
+
+// Conexion a BD
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', {
+        useNewUrlParser: true
+    })
+    .then(() => {
+        console.log("Base de datos: \x1b[32m%s\x1b[0m", " online")
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+
+
+
+// Rutas      , request,response, next
+
+app.get('/', (req, res, next) => {
+
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Peticion realizada correctamente'
+    });
+
+});
+
+
+
+// Escuchar peticiones
+app.listen(3000, () => {
+    console.log('Express Server corriendo en puerto 3000: \x1b[32m%s\x1b[0m', 'ONLINE');
+});
+
+// para probar
